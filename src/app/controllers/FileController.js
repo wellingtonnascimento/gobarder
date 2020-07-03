3bd3c326aa69dd833ff0b413718172522f81bbff
Wellington N <wellingto_07@hotmail.com>
2020-07-03 10:12:52 -0300

    create file db conect

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,6 +1,19 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json(req.file);
+    const { originalname: name, filename: path } = req.file;
+
+    try {
+      const file = await File.create({
+        name,
+        path,
+      });
+
+      return res.json(file);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
