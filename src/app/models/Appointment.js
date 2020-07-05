7fe928e669e11b5fa25e2411cb6107ae37176ce2
Wellington N <wellingto_07@hotmail.com>
2020-07-05 08:19:35 -0300

    correção associate user_id

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -16,7 +16,7 @@ class Appointment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'usei' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
