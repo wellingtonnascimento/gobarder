092571195ffa77242fa9409e47f6208278b04ed7
Wellington N <wellingto_07@hotmail.com>
2020-07-07 13:38:38 -0300

    verificação se usuario e prestador de serviço

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,7 +1,16 @@
+import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class ScheduleController {
   async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
     return res.json();
   }
 }
