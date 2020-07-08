9bf6047eb77ceecb9ba30f67a5b99dc0f9b75c5a
Wellington N <wellingto_07@hotmail.com>
2020-07-08 10:18:41 -0300

    validação de provider

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notifications' });
+    }
+
+    return res.json();
+  }
+}
+
+export default new NotificationController();
