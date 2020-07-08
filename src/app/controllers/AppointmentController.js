b5473794995f68ba792a1d0918224973f4093bdf
Wellington N <wellingto_07@hotmail.com>
2020-07-08 09:53:08 -0300

    notificando novos agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -90,6 +92,21 @@ class AppointmentController {
         user_id: req.userId,
         provider_id,
         date,
+      });
+
+      /**
+       * Notify appointment provider
+       */
+      const user = await User.findByPk(req.userId);
+      const formattedDate = format(
+        hourStrat,
+        "'dia' dd 'de' MMM', ás' H:mm'h'",
+        { locale: pt }
+      );
+
+      await Notification.create({
+        content: `Novo Agendamento de ${user.name} para ${formattedDate}`,
+        user: provider_id,
       });
 
       return res.json(appointment);
