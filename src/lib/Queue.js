1aec6bf689cd4a8641ff9a84d6a151f13b77d7b0
Wellington N <wellingto_07@hotmail.com>
2020-07-14 10:46:01 -0300

    class que gerenciar a fila de mail

---
+++ src/lib/Queue.js
@@ -0,0 +1,38 @@
+import Bee from 'bee-queue';
+import CancellationMall from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMall];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.crateJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach((job) => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
