5729b15ade28f9b2350bc4198071fbdfa2b9126d
Wellington N <wellingto_07@hotmail.com>
2020-07-15 10:27:03 -0300

    validation that is passing date

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,5 +1,12 @@
 class AvailableController {
   async index(req, res) {
+
+    const {date} req.query;
+
+    if(!date){
+      return res.status(400).json({error: 'Invalid date'})
+    }
+
     return res.json();
   }
 }
