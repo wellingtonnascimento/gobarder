488d53b1ad68f3e09fd58f4cdb321b4c28b5e4c1
Wellington N <wellingto_07@hotmail.com>
2020-07-15 10:56:06 -0300

    horarios de trabalho do provider

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -22,7 +22,22 @@ class AvailableController {
       },
     });
 
-    return res.json(appointment);
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    return res.json();
   }
 }
 
