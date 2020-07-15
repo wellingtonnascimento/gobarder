1dbe17f95472b7d38e7b0d5ca8b959fc4762b89c
Wellington N <wellingto_07@hotmail.com>
2020-07-15 11:25:12 -0300

    list of available times

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -20,7 +20,7 @@ class AvailableController {
 
     const searchDate = Number(date);
 
-    const appointment = await Appointment.findAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
@@ -55,7 +55,9 @@ class AvailableController {
       return {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-        available: isAfter(value, new Date()),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find((a) => format(a.date, 'HH:mm') === time),
       };
     });
 
