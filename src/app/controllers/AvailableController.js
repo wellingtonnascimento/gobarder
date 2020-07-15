845e21390e5b788fb16589cdcc89ff2bb37c8118
Wellington N <wellingto_07@hotmail.com>
2020-07-15 10:42:15 -0300

    correção chamada do id provider

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,13 +1,28 @@
+import { startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
 class AvailableController {
   async index(req, res) {
+    const { date } = req.query;
 
-    const {date} req.query;
-
-    if(!date){
-      return res.status(400).json({error: 'Invalid date'})
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
     }
 
-    return res.json();
+    const searchDate = Number(date);
+
+    const appointment = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    return res.json(appointment);
   }
 }
 
