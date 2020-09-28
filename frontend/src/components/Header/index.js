ca9caa70289939f98b0d217e18cc91ba005d93d9
Wellington N <wellingto_07@hotmail.com>
2020-09-28 10:28:15 -0300

    create header

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logoheader.svg';
+
+import { Container, Content, Profile } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Wellington Nacimento</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/56/abott@adorable.png"
+              alt="Wellington Nascimento"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
+
+export default Header;
