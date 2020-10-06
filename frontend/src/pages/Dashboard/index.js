a12bf27db894d2d7aaa5a18b49a4d43739302979
Wellington N <wellingto_07@hotmail.com>
2020-10-06 01:03:52 -0300

    styled dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import api from '../../services/api';
 
+import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
+
+import { Container, Time } from './styles';
+
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size="36" color="#fff" />
+        </button>
+        <strong>06 de Outubro</strong>
+        <button type="button">
+          <MdChevronRight size="36" color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Wellington</span>
+        </Time>
+        <Time>
+          <strong available>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
